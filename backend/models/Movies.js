25a8c49fc91baec0771fbd305bbbeb2d1dd12e42
joelvilcav <jvilcav29@gmail.com>
2023-11-02 17:55:09 -0500

    movies model added

---
+++ backend/models/Movies.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const movieSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    decription: {
+      type: String,
+      trim: true,
+    },
+    url: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Movie = mongoose.model('Movie', movieSchema);
+export default Movie;
